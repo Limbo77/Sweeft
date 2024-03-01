4e0f406daef48c50f272f5ae29f60e7c4ed2dd06
Vako Ts <vtsiklauri7@gmail.com>
2024-03-01 11:44:33 +0400

    Added Different Solution T1
    Wrote solution to the problem using built-in tools of java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import FirstTask.FirstTask;
 
-public class Main {
-    public static void main(String[] args) {
-        //TestCase for FirstTask
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
+public class Main {
+
+    //In this branch, I will be doing the same tasks but with built-in java tools
+    public static void main(String[] args)  {
+        //TestCase for FirstTask
+        /*
         String exp = "3 + 4 * 2";
-        System.out.println(FirstTask.evaluateExpression(exp));
+        try{
+           FirstTask.evaluateExpression(exp);
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
+        }
+        */
     }
 }
